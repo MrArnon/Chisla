a75bf76d87b0e8906eb343f4347c8624d96bc0e9
MrArnon <32636989+MrArnon@users.noreply.github.com>
2017-11-04 14:06:03 +0300

    laba 2 is done

--- CM_laba1/CM_laba1/Form1.Designer.cs
+++ CM_laba1/CM_laba1/Form1.Designer.cs
@@ -28,126 +28,164 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.Title title3 = new System.Windows.Forms.DataVisualization.Charting.Title();
-            this.Graphic = new System.Windows.Forms.DataVisualization.Charting.Chart();
-            this.Label_stepen = new System.Windows.Forms.Label();
-            this.Stepen_box = new System.Windows.Forms.TextBox();
-            this.Start_but = new System.Windows.Forms.Button();
-            this.A_box = new System.Windows.Forms.TextBox();
-            this.B_box = new System.Windows.Forms.TextBox();
-            this.Label_interval = new System.Windows.Forms.Label();
-            this.Vivod_text = new System.Windows.Forms.TextBox();
-            ((System.ComponentModel.ISupportInitialize)(this.Graphic)).BeginInit();
-            this.SuspendLayout();
-            // 
-            // Graphic
-            // 
-            chartArea3.Name = "ChartArea1";
-            this.Graphic.ChartAreas.Add(chartArea3);
-            legend3.Name = "Legend1";
-            this.Graphic.Legends.Add(legend3);
-            this.Graphic.Location = new System.Drawing.Point(-1, 2);
-            this.Graphic.Name = "Graphic";
-            this.Graphic.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Bright;
-            series5.ChartArea = "ChartArea1";
-            series5.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
-            series5.Legend = "Legend1";
-            series5.LegendText = "Sqrt(3 + x^2) / Sqrt(5 + 7 * x^2)";
-            series5.Name = "Func_graf";
-            series6.ChartArea = "ChartArea1";
-            series6.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
-            series6.Legend = "Legend1";
-            series6.LegendText = "Полином";
-            series6.Name = "Polinom_graf";
-            this.Graphic.Series.Add(series5);
-            this.Graphic.Series.Add(series6);
-            this.Graphic.Size = new System.Drawing.Size(850, 300);
-            this.Graphic.TabIndex = 0;
-            this.Graphic.Text = "График";
-            title3.Name = "Graphic_func";
-            this.Graphic.Titles.Add(title3);
-            // 
-            // Label_stepen
-            // 
-            this.Label_stepen.AutoSize = true;
-            this.Label_stepen.Location = new System.Drawing.Point(12, 404);
-            this.Label_stepen.Name = "Label_stepen";
-            this.Label_stepen.Size = new System.Drawing.Size(146, 13);
-            this.Label_stepen.TabIndex = 1;
-            this.Label_stepen.Text = "Ввидите степень полинома";
-            // 
-            // Stepen_box
-            // 
-            this.Stepen_box.Location = new System.Drawing.Point(164, 401);
-            this.Stepen_box.Name = "Stepen_box";
-            this.Stepen_box.Size = new System.Drawing.Size(33, 20);
-            this.Stepen_box.TabIndex = 2;
-            // 
-            // Start_but
-            // 
-            this.Start_but.Location = new System.Drawing.Point(280, 394);
-            this.Start_but.Name = "Start_but";
-            this.Start_but.Size = new System.Drawing.Size(92, 33);
-            this.Start_but.TabIndex = 3;
-            this.Start_but.Text = "Построить";
-            this.Start_but.UseVisualStyleBackColor = true;
-            this.Start_but.Click += new System.EventHandler(this.Start_but_Click);
-            // 
-            // A_box
-            // 
-            this.A_box.Location = new System.Drawing.Point(15, 350);
-            this.A_box.Name = "A_box";
-            this.A_box.Size = new System.Drawing.Size(33, 20);
-            this.A_box.TabIndex = 4;
-            // 
-            // B_box
-            // 
-            this.B_box.Location = new System.Drawing.Point(164, 350);
-            this.B_box.Name = "B_box";
-            this.B_box.Size = new System.Drawing.Size(33, 20);
-            this.B_box.TabIndex = 5;
-            // 
-            // Label_interval
-            // 
-            this.Label_interval.AutoSize = true;
-            this.Label_interval.Location = new System.Drawing.Point(12, 323);
-            this.Label_interval.Name = "Label_interval";
-            this.Label_interval.Size = new System.Drawing.Size(151, 13);
-            this.Label_interval.TabIndex = 6;
-            this.Label_interval.Text = "Ввидите границы интервала";
-            // 
-            // Vivod_text
-            // 
-            this.Vivod_text.Location = new System.Drawing.Point(551, 347);
-            this.Vivod_text.Name = "Vivod_text";
-            this.Vivod_text.Size = new System.Drawing.Size(170, 20);
-            this.Vivod_text.TabIndex = 7;
-            this.Vivod_text.Visible = false;
-            // 
-            // Form_for_graphic
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(844, 461);
-            this.Controls.Add(this.Vivod_text);
-            this.Controls.Add(this.Label_interval);
-            this.Controls.Add(this.B_box);
-            this.Controls.Add(this.A_box);
-            this.Controls.Add(this.Start_but);
-            this.Controls.Add(this.Stepen_box);
-            this.Controls.Add(this.Label_stepen);
-            this.Controls.Add(this.Graphic);
-            this.MaximumSize = new System.Drawing.Size(860, 500);
-            this.MinimumSize = new System.Drawing.Size(860, 500);
-            this.Name = "Form_for_graphic";
-            this.Text = "График функции";
-            ((System.ComponentModel.ISupportInitialize)(this.Graphic)).EndInit();
-            this.ResumeLayout(false);
-            this.PerformLayout();
+			System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+			System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+			System.Windows.Forms.DataVisualization.Charting.Series series9 = new System.Windows.Forms.DataVisualization.Charting.Series();
+			System.Windows.Forms.DataVisualization.Charting.Series series10 = new System.Windows.Forms.DataVisualization.Charting.Series();
+			System.Windows.Forms.DataVisualization.Charting.Series series11 = new System.Windows.Forms.DataVisualization.Charting.Series();
+			System.Windows.Forms.DataVisualization.Charting.Series series12 = new System.Windows.Forms.DataVisualization.Charting.Series();
+			System.Windows.Forms.DataVisualization.Charting.Title title3 = new System.Windows.Forms.DataVisualization.Charting.Title();
+			this.Graphic = new System.Windows.Forms.DataVisualization.Charting.Chart();
+			this.Label_stepen = new System.Windows.Forms.Label();
+			this.Stepen_box = new System.Windows.Forms.TextBox();
+			this.Polin_but = new System.Windows.Forms.Button();
+			this.A_box = new System.Windows.Forms.TextBox();
+			this.B_box = new System.Windows.Forms.TextBox();
+			this.Label_interval = new System.Windows.Forms.Label();
+			this.Vivod_text = new System.Windows.Forms.TextBox();
+			this.Mnogo_but = new System.Windows.Forms.Button();
+			this.Table_but = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.Graphic)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// Graphic
+			// 
+			chartArea3.Name = "ChartArea1";
+			this.Graphic.ChartAreas.Add(chartArea3);
+			legend3.Name = "Legend1";
+			this.Graphic.Legends.Add(legend3);
+			this.Graphic.Location = new System.Drawing.Point(-1, 2);
+			this.Graphic.Name = "Graphic";
+			this.Graphic.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Pastel;
+			series9.ChartArea = "ChartArea1";
+			series9.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+			series9.Legend = "Legend1";
+			series9.LegendText = "Sqrt(3 + x^2) / Sqrt(5 + 7 * x^2)";
+			series9.Name = "Func_graf";
+			series10.ChartArea = "ChartArea1";
+			series10.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+			series10.Legend = "Legend1";
+			series10.LegendText = "Полином";
+			series10.Name = "Polinom_graf";
+			series11.ChartArea = "ChartArea1";
+			series11.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+			series11.Legend = "Legend1";
+			series11.LegendText = "Многочлен";
+			series11.Name = "Mnogo_graf";
+			series12.ChartArea = "ChartArea1";
+			series12.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+			series12.Legend = "Legend1";
+			series12.LegendText = "Таблица";
+			series12.Name = "Table_graf";
+			this.Graphic.Series.Add(series9);
+			this.Graphic.Series.Add(series10);
+			this.Graphic.Series.Add(series11);
+			this.Graphic.Series.Add(series12);
+			this.Graphic.Size = new System.Drawing.Size(850, 300);
+			this.Graphic.TabIndex = 0;
+			this.Graphic.Text = "График";
+			title3.Name = "Graphic_func";
+			this.Graphic.Titles.Add(title3);
+			// 
+			// Label_stepen
+			// 
+			this.Label_stepen.AutoSize = true;
+			this.Label_stepen.Location = new System.Drawing.Point(12, 404);
+			this.Label_stepen.Name = "Label_stepen";
+			this.Label_stepen.Size = new System.Drawing.Size(146, 13);
+			this.Label_stepen.TabIndex = 1;
+			this.Label_stepen.Text = "Ввидите степень полинома";
+			// 
+			// Stepen_box
+			// 
+			this.Stepen_box.Location = new System.Drawing.Point(164, 401);
+			this.Stepen_box.Name = "Stepen_box";
+			this.Stepen_box.Size = new System.Drawing.Size(33, 20);
+			this.Stepen_box.TabIndex = 2;
+			// 
+			// Polin_but
+			// 
+			this.Polin_but.Location = new System.Drawing.Point(280, 394);
+			this.Polin_but.Name = "Polin_but";
+			this.Polin_but.Size = new System.Drawing.Size(92, 33);
+			this.Polin_but.TabIndex = 3;
+			this.Polin_but.Text = "Полином";
+			this.Polin_but.UseVisualStyleBackColor = true;
+			this.Polin_but.Click += new System.EventHandler(this.Start_but_Click);
+			// 
+			// A_box
+			// 
+			this.A_box.Location = new System.Drawing.Point(15, 350);
+			this.A_box.Name = "A_box";
+			this.A_box.Size = new System.Drawing.Size(33, 20);
+			this.A_box.TabIndex = 4;
+			// 
+			// B_box
+			// 
+			this.B_box.Location = new System.Drawing.Point(164, 350);
+			this.B_box.Name = "B_box";
+			this.B_box.Size = new System.Drawing.Size(33, 20);
+			this.B_box.TabIndex = 5;
+			// 
+			// Label_interval
+			// 
+			this.Label_interval.AutoSize = true;
+			this.Label_interval.Location = new System.Drawing.Point(12, 323);
+			this.Label_interval.Name = "Label_interval";
+			this.Label_interval.Size = new System.Drawing.Size(151, 13);
+			this.Label_interval.TabIndex = 6;
+			this.Label_interval.Text = "Ввидите границы интервала";
+			// 
+			// Vivod_text
+			// 
+			this.Vivod_text.Location = new System.Drawing.Point(551, 347);
+			this.Vivod_text.Name = "Vivod_text";
+			this.Vivod_text.Size = new System.Drawing.Size(170, 20);
+			this.Vivod_text.TabIndex = 7;
+			this.Vivod_text.Visible = false;
+			// 
+			// Mnogo_but
+			// 
+			this.Mnogo_but.Location = new System.Drawing.Point(410, 394);
+			this.Mnogo_but.Name = "Mnogo_but";
+			this.Mnogo_but.Size = new System.Drawing.Size(92, 33);
+			this.Mnogo_but.TabIndex = 8;
+			this.Mnogo_but.Text = "Многочлен";
+			this.Mnogo_but.UseVisualStyleBackColor = true;
+			this.Mnogo_but.Click += new System.EventHandler(this.Mnogo_but_Click);
+			// 
+			// Table_but
+			// 
+			this.Table_but.Location = new System.Drawing.Point(539, 394);
+			this.Table_but.Name = "Table_but";
+			this.Table_but.Size = new System.Drawing.Size(92, 33);
+			this.Table_but.TabIndex = 9;
+			this.Table_but.Text = "Таблица";
+			this.Table_but.UseVisualStyleBackColor = true;
+			this.Table_but.Click += new System.EventHandler(this.Table_but_Click);
+			// 
+			// Form_for_graphic
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(844, 461);
+			this.Controls.Add(this.Table_but);
+			this.Controls.Add(this.Mnogo_but);
+			this.Controls.Add(this.Vivod_text);
+			this.Controls.Add(this.Label_interval);
+			this.Controls.Add(this.B_box);
+			this.Controls.Add(this.A_box);
+			this.Controls.Add(this.Polin_but);
+			this.Controls.Add(this.Stepen_box);
+			this.Controls.Add(this.Label_stepen);
+			this.Controls.Add(this.Graphic);
+			this.MaximumSize = new System.Drawing.Size(860, 500);
+			this.MinimumSize = new System.Drawing.Size(860, 500);
+			this.Name = "Form_for_graphic";
+			this.Text = "График функции";
+			((System.ComponentModel.ISupportInitialize)(this.Graphic)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
 
         }
 
@@ -156,11 +194,13 @@
         private System.Windows.Forms.DataVisualization.Charting.Chart Graphic;
         private System.Windows.Forms.Label Label_stepen;
         private System.Windows.Forms.TextBox Stepen_box;
-        private System.Windows.Forms.Button Start_but;
+        private System.Windows.Forms.Button Polin_but;
         private System.Windows.Forms.TextBox A_box;
         private System.Windows.Forms.TextBox B_box;
         private System.Windows.Forms.Label Label_interval;
         private System.Windows.Forms.TextBox Vivod_text;
-    }
+		private System.Windows.Forms.Button Mnogo_but;
+		private System.Windows.Forms.Button Table_but;
+	}
 }
 
--- CM_laba1/CM_laba1/Form1.cs
+++ CM_laba1/CM_laba1/Form1.cs
@@ -55,12 +55,7 @@ namespace CM_laba1
                 Graphic.ChartAreas[0].CursorX.IsUserSelectionEnabled = true;// выбор интервалов для масштабирования
                 Graphic.ChartAreas[0].AxisX.ScaleView.Zoomable = true;
                 Graphic.ChartAreas[0].AxisX.ScrollBar.IsPositionedInside = true;
-
-                //масштабируем по У
-                // Graphic.ChartAreas[0].AxisY.Minimum = -1;
-                // Graphic.ChartAreas[0].AxisY.Maximum = 1;
-                /// Graphic.ChartAreas[0].AxisY.ScaleView.Zoom(-1, 1);
-                Graphic.ChartAreas[0].CursorY.IsUserEnabled = true;
+				Graphic.ChartAreas[0].CursorY.IsUserEnabled = true;
                 Graphic.ChartAreas[0].CursorY.IsUserSelectionEnabled = true;// выбор интервалов для масштабирования
                 Graphic.ChartAreas[0].AxisY.ScaleView.Zoomable = true;
                 Graphic.ChartAreas[0].AxisY.ScrollBar.IsPositionedInside = true;
@@ -83,6 +78,144 @@ namespace CM_laba1
                 //
             }
         }
-    }
+
+		private void Mnogo_but_Click(object sender, EventArgs e)
+		{
+
+			Graphic.Series[0].Points.Clear();
+			Graphic.Series[2].Points.Clear();
+			Function_graphics graf = new Function_graphics();
+			try
+			{
+				graf.n = Convert.ToInt32(Stepen_box.Text);
+				graf.a = Convert.ToDouble(A_box.Text);
+				graf.b = Convert.ToDouble(B_box.Text);
+				if (graf.a >= graf.b)
+				{
+					MessageBox.Show("Неверный ввод");
+					A_box.Text = string.Empty;
+					B_box.Text = string.Empty;
+					Stepen_box.Text = string.Empty;
+					graf.flag = false;
+				}
+			}
+			catch (Exception)
+			{
+				MessageBox.Show("Неверный ввод");
+				A_box.Text = string.Empty;
+				B_box.Text = string.Empty;
+				Stepen_box.Text = string.Empty;
+				graf.flag = false;
+			}
+			if (graf.flag)
+			{
+				graf.H();
+
+				//масштабируем по Х
+				//Graphic.ChartAreas[0].AxisX.Minimum = -2;
+				//Graphic.ChartAreas[0].AxisX.Maximum = 3;
+				// Graphic.ChartAreas[0].AxisX.ScaleView.Zoom(-2, 3);
+				Graphic.ChartAreas[0].CursorX.IsUserEnabled = true;
+				Graphic.ChartAreas[0].CursorX.IsUserSelectionEnabled = true;// выбор интервалов для масштабирования
+				Graphic.ChartAreas[0].AxisX.ScaleView.Zoomable = true;
+				Graphic.ChartAreas[0].AxisX.ScrollBar.IsPositionedInside = true;
+				Graphic.ChartAreas[0].CursorY.IsUserEnabled = true;
+				Graphic.ChartAreas[0].CursorY.IsUserSelectionEnabled = true;// выбор интервалов для масштабирования
+				Graphic.ChartAreas[0].AxisY.ScaleView.Zoomable = true;
+				Graphic.ChartAreas[0].AxisY.ScrollBar.IsPositionedInside = true;
+
+
+				Graphic.Series[0].Points.Clear();
+				Graphic.Series[2].Points.Clear();
+				for (double i = graf.a; i <= graf.b; i += graf.h)
+				{
+					for (double j = i; (j <= i + graf.h) && j < graf.b; j += (double)(graf.h / graf.m))
+					{
+
+						Graphic.Series[0].Points.AddXY(j, graf.func(j));
+						
+
+					}
+
+
+				}
+			
+				for (double g = graf.a; g < graf.b; g += 0.001)
+				{
+					Graphic.Series[2].Points.AddXY(g, graf.polynomial(g));
+				}
+				//
+				//Vivod_text.Text=Convert.ToString( graf.func(-1));
+				//
+			}
+		}
+
+		private void Table_but_Click(object sender, EventArgs e)
+		{
+			Graphic.Series[0].Points.Clear();
+			Graphic.Series[3].Points.Clear();
+			Function_graphics graf = new Function_graphics();
+			try
+			{
+				graf.n = Convert.ToInt32(Stepen_box.Text);
+				graf.a = Convert.ToDouble(A_box.Text);
+				graf.b = Convert.ToDouble(B_box.Text);
+				if (graf.a >= graf.b)
+				{
+					MessageBox.Show("Неверный ввод");
+					A_box.Text = string.Empty;
+					B_box.Text = string.Empty;
+					Stepen_box.Text = string.Empty;
+					graf.flag = false;
+				}
+			}
+			catch (Exception)
+			{
+				MessageBox.Show("Неверный ввод");
+				A_box.Text = string.Empty;
+				B_box.Text = string.Empty;
+				Stepen_box.Text = string.Empty;
+				graf.flag = false;
+			}
+			if (graf.flag)
+			{
+				graf.H();
+
+				//масштабируем по Х
+				//Graphic.ChartAreas[0].AxisX.Minimum = -2;
+				//Graphic.ChartAreas[0].AxisX.Maximum = 3;
+				// Graphic.ChartAreas[0].AxisX.ScaleView.Zoom(-2, 3);
+				Graphic.ChartAreas[0].CursorX.IsUserEnabled = true;
+				Graphic.ChartAreas[0].CursorX.IsUserSelectionEnabled = true;// выбор интервалов для масштабирования
+				Graphic.ChartAreas[0].AxisX.ScaleView.Zoomable = true;
+				Graphic.ChartAreas[0].AxisX.ScrollBar.IsPositionedInside = true;
+				Graphic.ChartAreas[0].CursorY.IsUserEnabled = true;
+				Graphic.ChartAreas[0].CursorY.IsUserSelectionEnabled = true;// выбор интервалов для масштабирования
+				Graphic.ChartAreas[0].AxisY.ScaleView.Zoomable = true;
+				Graphic.ChartAreas[0].AxisY.ScrollBar.IsPositionedInside = true;
+
+
+				Graphic.Series[0].Points.Clear();
+				Graphic.Series[3].Points.Clear();
+				for (double i = graf.a; i <= graf.b; i += graf.h)
+				{
+					for (double j = i; (j <= i + graf.h) && j < graf.b; j += (double)(graf.h / graf.m))
+					{
+
+						Graphic.Series[0].Points.AddXY(j, graf.func(j));
+
+
+					}
+				}
+
+				graf.tabel = new double[graf.n + 2, graf.n + 2];
+				for (double g = graf.a; g < graf.b; g += 0.001)
+				{
+					Graphic.Series[3].Points.AddXY(g, graf.graf_table(g));
+				}
+				//insert your code
+			}
+			}
+	}
 }
 
